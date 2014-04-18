1226e10a6da627c3939dc71a1bdf4e5d7a7be357
renubora <renubora@ischool.berkeley.edu>
2014-04-18 11:53:43 -0700

    couldn't modify build so will use this file

--- firsttest.py
+++ firsttest.py
@@ -6,25 +6,31 @@
 
 __author__="Renu"
 
+
+import unittest
 import checkfile
 
 
+
+
+class TestHeader(unittest.TestCase):
+    def test_checkheader(self):
+
+        #print checkfile.checkheader()
+        #Failing test
+        #self.assertEqual(checkfile.checkheader(),'Dcustomer_ID')
+
+        self.assertEqual(checkfile.checkheader(),'customer_ID')
+
+
+
+
+
+
 if __name__ == '__main__':
-   
-# Just to test the build works
-	print "Build tool seems to work."
+    unittest.main()
 
 
-#FailingTest
-#assert checkfile() == 'cust'
 
-#PassingTest
-	assert checkfile.checkheader() == 'customer_ID'
-	print "test passed"
 
-# Test unit test now with snap-ci
-#assert "customer_ID,shopping_pt,record_type,day,time,state,location,group_size,homeowner,car_age,car_value,risk_factor,age_oldest,age_youngest,married_couple,C_previous,duration_previous,A,B,C,D,E,F,G,cost,total_change,cum_change,old_A,old_B,old_C,old_D,old_E,old_F,old_G,old_C_previous" = OutputFirstHeaderItem()
-
-# will do this later- switch to unit testing for project- figure out
-# Pycharm  maybe (though that might be too automated. Pyscripter seems too automated for learning but we'll see)
 
